d7594d11f60f98ee3091c543b3ed0c6e5c7533a1
Axel Huebl <axel.huebl@plasma.ninja>
2016-04-19 16:25:35 -0700

    Bash: h5compress
    Add a serial HDF5 compress script, too.

---
+++ bash-h5compress.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# Copyright 2015-2016 Axel Huebl
+#
+# ISC License
+#
+if [ -z "$1" ]
+then
+  echo "This script compresses all found HDF5 files in a given"
+  echo "directory recursively."
+  echo "Although compression and move are done sequentially,"
+  echo "try not to abort it half-way (e.g., run in a `screen`)."
+  echo ""
+  echo "$0 /path/to/dir"
+  exit 1
+fi
+
+find $1 -name "*.h5" | xargs -n1 -P8 -I{} \
+    sh -c 'echo "compress $1 ..." && \
+    h5repack -i $1 -o $1.gz -f GZIP=1 && mv $1.gz $1' _ {}
